77b1ad23da75bf2eb7952c78f1da7d1cb02b5b20
jyothirmai1993 <jyothirmai1993@users.noreply.github.com>
2019-08-12 15:40:22 +0200

    subtract and multiply functions available

--- git-test-multiply.js
+++ git-test-multiply.js
@@ -1,4 +1,7 @@
 // multiply.js
+
+//return product of two numbers
 function multiply(a, b) {
+	return a*b
     // TODO return
 };
--- git-test-subtract.js
+++ git-test-subtract.js
@@ -1,4 +1,7 @@
 // subtract.js
+
+// return substraction of two numbers
 function subtract(a, b) {
+	return a-b
     // TODO return
 };
